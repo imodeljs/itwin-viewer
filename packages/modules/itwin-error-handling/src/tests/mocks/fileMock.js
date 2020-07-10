ffb5fdcee2144311206f5171709bc10ac2962b6d
Caleb Shafer <31107829+calebmshafer@users.noreply.github.com>
2020-07-10 16:31:02 -0400

    Remove extra policy and dup header

--- packages/modules/itwin-error-handling/src/tests/mocks/fileMock.js
+++ packages/modules/itwin-error-handling/src/tests/mocks/fileMock.js
@@ -2,7 +2,4 @@
 * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
 * See LICENSE.md in the project root for license terms and full copyright notice.
 *--------------------------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------------------------
-|  $Copyright: (c) 2019 Bentley Systems, Incorporated. All rights reserved. $
- *--------------------------------------------------------------------------------------------*/
 module.exports = "test-file-stub";
