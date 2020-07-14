87e4c80cf92ec625a8fd9a8770e76abfae70ec7b
Kevin Krisfalusy <6283674+kckst8@users.noreply.github.com>
2020-07-14 11:47:57 -0400

    test precommit

--- common/scripts/pre-commit.js
+++ common/scripts/pre-commit.js
@@ -1,18 +1,17 @@
 const lintStaged = require("lint-staged");
-console.log("linting");
-lintStaged({debug: true, config: {
+return lintStaged({debug: true, config: {
   "*.{ts,tsx}": [
-    "node ../../../common/scripts/copyright-linter.js --",
-    "node --max_old_space_size=4096 ./node_modules/eslint/bin/eslint.js --config ../../../common/config/.eslintrc.ts.autofix.json --ignore-path ../../../common/config/.eslintignore --fix",
-    "prettier --write --config ../../../.prettierrc --ignore-path ../../../.prettierignore",
-    "node --max_old_space_size=4096 ./node_modules/eslint/bin/eslint.js --config ../../../common/config/.eslintrc.ts.json --ignore-path ../../../common/config/.eslintignore --color"
+    "node ./copyright-linter.js --",
+    "node --max_old_space_size=4096 ./node_modules/eslint/bin/eslint.js --config ../config/.eslintrc.ts.autofix.json --ignore-path ..//config/.eslintignore --fix",
+    "prettier --write --config ../../.prettierrc --ignore-path ../../.prettierignore",
+    "node --max_old_space_size=4096 ./node_modules/eslint/bin/eslint.js --config ../config/.eslintrc.ts.json --ignore-path ../config/.eslintignore --color"
   ],
   "*.{md,json}": [
-    "prettier --write --config ../../../.prettierrc --ignore-path ../../../.prettierignore"
+    "prettier --write --config ../../.prettierrc --ignore-path ../../.prettierignore"
   ],
   "*.{scss,css}": [
-    "node ../../../common/config/scripts/copyright-linter.js --",
-    "stylelint --fix --config ../../../.stylelintrc"
+    "node ../config/scripts/copyright-linter.js --",
+    "stylelint --fix --config ../../.stylelintrc"
   ]
 }});
 
