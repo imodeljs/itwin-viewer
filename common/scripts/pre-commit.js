aa1ec0ab7074d3ad15634b96ec160faba579d9cb
Kevin Krisfalusy <6283674+kckst8@users.noreply.github.com>
2020-07-14 12:43:05 -0400

    test

--- common/scripts/pre-commit.js
+++ common/scripts/pre-commit.js
@@ -1,17 +1,19 @@
 const lintStaged = require("lint-staged");
-return lintStaged({debug: true, config: {
-  "*.{ts,tsx}": [
-    "node ./common/scripts/copyright-linter.js --",
-    "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.autofix.json --ignore-path ./.eslintignore --fix",
-    "prettier --write --config ./.prettierrc --ignore-path ./.prettierignore",
-    "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.json --ignore-path ./.eslintignore --color"
-  ],
-  "*.{md,json}": [
-    "prettier --write --config ./.prettierrc --ignore-path ./.prettierignore"
-  ],
-  "*.{scss,css}": [
-    "node ./common/scripts/copyright-linter.js --",
-    "stylelint --fix --config ./.stylelintrc"
-  ]
-}});
+module.exports = async function () {
+  return lintStaged({debug: true, config: {
+    "*.{ts,tsx}": [
+      "node ./common/scripts/copyright-linter.js --",
+      "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.autofix.json --ignore-path ./.eslintignore --fix",
+      "prettier --write --config ./.prettierrc --ignore-path ./.prettierignore",
+      "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.json --ignore-path ./.eslintignore --color"
+    ],
+    "*.{md,json}": [
+      "prettier --write --config ./.prettierrc --ignore-path ./.prettierignore"
+    ],
+    "*.{scss,css}": [
+      "node ./common/scripts/copyright-linter.js --",
+      "stylelint --fix --config ./.stylelintrc"
+    ]
+  }});
+}
 
