65205232244bc4f2990d23156cf1a030946f9f7f
Kevin Krisfalusy <6283674+kckst8@users.noreply.github.com>
2020-07-14 11:37:27 -0400

    add config

--- common/scripts/pre-commit.js
+++ common/scripts/pre-commit.js
@@ -1,4 +1,18 @@
 const lintStaged = require("lint-staged");
 console.log("linting");
-lintStaged({debug: true});
+lintStaged({debug: true, config: {
+  "*.{ts,tsx}": [
+    "node ../../../common/scripts/copyright-linter.js --",
+    "node --max_old_space_size=4096 ./node_modules/eslint/bin/eslint.js --config ../../../common/config/.eslintrc.ts.autofix.json --ignore-path ../../../common/config/.eslintignore --fix",
+    "prettier --write --config ../../../.prettierrc --ignore-path ../../../.prettierignore",
+    "node --max_old_space_size=4096 ./node_modules/eslint/bin/eslint.js --config ../../../common/config/.eslintrc.ts.json --ignore-path ../../../common/config/.eslintignore --color"
+  ],
+  "*.{md,json}": [
+    "prettier --write --config ../../../.prettierrc --ignore-path ../../../.prettierignore"
+  ],
+  "*.{scss,css}": [
+    "node ../../../common/config/scripts/copyright-linter.js --",
+    "stylelint --fix --config ../../../.stylelintrc"
+  ]
+}});
 
