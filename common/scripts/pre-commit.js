c24d15d679d608d29ca7d4f58f3f9cfee6b37132
Kevin Krisfalusy <6283674+kckst8@users.noreply.github.com>
2020-07-14 12:47:46 -0400

    test

--- common/scripts/pre-commit.js
+++ common/scripts/pre-commit.js
@@ -1,6 +1,6 @@
 const lintStaged = require("lint-staged");
 
-const ret = lintStaged({debug: true, config: {
+const ret = await lintStaged({debug: true, config: {
     "*.{ts,tsx}": [
       "node ./common/scripts/copyright-linter.js --",
       "node --max_old_space_size=4096 ./common/scripts/node_modules/eslint/bin/eslint.js --config ./common/scripts/.eslintrc.ts.autofix.json --ignore-path ./.eslintignore --fix",
