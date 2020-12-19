e72d9cdd5be4e4dd5962b386c632342789c3301f
Arun George <aruniverse@users.noreply.github.com>
2020-12-19 17:07:01 -0500

    fix scss so theres no overflow and can fit header and viewer perfectly

--- packages/modules/cra-template-itwin-viewer/template/src/App.tsx
+++ packages/modules/cra-template-itwin-viewer/template/src/App.tsx
@@ -62,7 +62,7 @@ const App: React.FC = () => {
   };
 
   return (
-    <div>
+    <div className="itwin-viewer-sample">
       <Header
         handleLogin={onLoginClick}
         loggedIn={isAuthorized}
