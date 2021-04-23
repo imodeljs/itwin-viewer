39280ff4da86dd0ecd4451c17c7de406541592d6
Stefan Retief <stefan.retief@bentley.com>
2021-04-23 07:41:36 -0700

    rush change

--- packages/modules/itwin-viewer-react/src/components/Viewer.tsx
+++ packages/modules/itwin-viewer-react/src/components/Viewer.tsx
@@ -2,10 +2,6 @@
  * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
  * See LICENSE.md in the project root for license terms and full copyright notice.
  *--------------------------------------------------------------------------------------------*/
-/*---------------------------------------------------------------------------------------------
- * Copyright (c) Bentley Systems, Incorporated. All rights reserved.
- * See LICENSE.md in the project root for license terms and full copyright notice.
- *--------------------------------------------------------------------------------------------*/
 
 import { ErrorBoundary } from "@bentley/itwin-error-handling-react";
 import React, { useEffect, useState } from "react";
